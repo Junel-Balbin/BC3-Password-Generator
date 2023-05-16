65708e755d458f3d854008b7586f8e208a0ccc0e
Junel <robin.lennuj@gmail.com>
2023-05-16 01:13:04 -0700

    Fixing JS structure.  Still needs a lot of work.

--- assets/script.js
+++ assets/script.js
@@ -1,90 +1,63 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0123456789"];
+var specialCharacters = ["!@#$%^&*+?></"];
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"]; 
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 
 
+function generatePassword(length) {
+  var password = "8 to 128 characters";
+  var allCharacters = numbers + specialCharacters + lowercase + uppercase;
 
+  for (var i = 0; i < password.length; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
 
+ var characters = "";
+ numbers = "0123456789";
 
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-var characters = "";
-numbers = "0123456789";
-
-var numbers = confirm("Do you want numbers?");
+ var numbers = confirm("Would you like numbers?");
   if (numbers) {
   characters += "0123456789";
-}
+ }
 
 
-var characters = "";
-specialCharacters = "!@#$%^&*";
+ var characters = "";
+ specialCharacters = "!@#$%^&*+?></";
 
-var specialCharacters = confirm("Would you like Special Characters?");
-  if (specialCharacters) {
-    characters += "!@#$%^&*";
+ var specialCharacters = confirm("Would you like Special Characters?");
+   if (specialCharacters) {
+    characters += "!@#$%^&*+?></";
   }
 
 
-var characters = "";
-lowercase = "abcdefghijklmnopqrstuvwxyz";
+ var characters = "";
+ lowercase = "abcdefghijklmnopqrstuvwxyz";
 
-var lowercase = confirm("Would you like Lowercases?");
+ var lowercase = confirm("Would you like Lowercases?");
   if (lowercase) {
     characters += "abcdefghijklmnopqrstuvwxyz";
   }
 
 
-var characters = "";
-uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var characters = "";
+ uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var uppercase = confirm("Would you like Uppercases?");
+ var uppercase = confirm("Would you like Uppercases?");
   if (uppercase) {
     characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
 
 
-
-
-
-
-
-
-for (var i = 0; i < password.length; i++) {
+ for (var i = 0; i < password.length; i++) {
   password += characters[Math.floor(Math.random() * characters.length)];
+ }
+
+  return password;
+
 }
-
-
-
-
-
-
-
 
 
   // Write password to the #password input
@@ -98,3 +71,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
