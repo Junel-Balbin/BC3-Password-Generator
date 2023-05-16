7a4ae72cb864374d7c2874ab0fffd9393544bdef
Junel <robin.lennuj@gmail.com>
2023-05-15 18:15:17 -0700

    Improved characters & worked on for-loop.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*",];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,8 +21,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// Numbers, Special Characters, Uppercase & Lowercases.
-// var numbers = "0", "1", "2", "3", "4", "5", "6", "7", "8", "9";
-//var specialCharacters = "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=";
-// var upperCase = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z";
-// var lowerCase = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z";
+// for-loop
+// for (var i = 0; i < password.length; i++) {
+//      password += characters[Math.floor(Math.random() * characters.length)];
+//    }
