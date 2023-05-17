c15d55028dae9af283455caa1e482118fa5ee2a8
Junel <robin.lennuj@gmail.com>
2023-05-17 02:29:21 -0700

    Finding & fixing mistakes.

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(length) {
+var password = "";
+var characters = "";
+var passwordLength = "";
+var numbers = "0123456789";
+var specialCharacters = "!@#$%^&*+?></";
+var lowercase = "abcdefghijklmnopqrstuvwxyz"; 
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ 
 
- var password = "";
- var characters = "";
- var passwordLength = "";
- var numbers = "0123456789";
- var specialCharacters = "!@#$%^&*+?></";
- var lowercase = "abcdefghijklmnopqrstuvwxyz"; 
- var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ function generatePassword() {
  
  var passwordLength = prompt("How many characters would you like between 8 - 128?");
-   if (passwordLength < 8 || passwordLength > 128) {
-      return "Please choose length between 8 - 128.";
-   }  else if (isNaN(passwordLength)) {
-      passwordLength = prompt("Please enter a valid number of length.");
-   }
-   else {
-      alert("Your password will be " + passwordLength + " characters long");
-   }
-
- var numbers = confirm("Would you like numbers?");
-  if (numbers === true) {
-    alert("Your password will have numbers.");  
- }
-
- var specialCharacters = confirm("Would you like Special Characters?");
-   if (specialCharacters === true) {
-    alert("Your password will have Special Characters.");
+  if (passwordLength < 8 || passwordLength > 128) {
+  return "Please select a length between 8 - 128.";
+  }  else if (isNaN(passwordLength)) {
+     passwordLength = prompt("Please enter a valid number of length.");
+  }
+     else {
+     alert("Your password will be " + passwordLength + " characters long");
   }
 
- var lowercase = confirm("Would you like Lowercases?");
-  if (lowercase === true) {
-    alert("Your password will have Lowercases.");
+
+ var includeNumbers = confirm("Would you like numbers?");
+  if (includeNumbers) {
+   characters += numbers;
+  alert("Your password will have numbers.");  
+  } 
+
+ var includeSpecialCharacters = confirm("Would you like Special Characters?");
+  if (includeSpecialCharacters) {
+   characters += specialCharacters;
+   alert("Your password will have Special Characters.");
   }
 
- var uppercase = confirm("Would you like Uppercases?");
-  if (uppercase === true) {
-    alert("Your password will have Uppercases");
+ var includeLowercase = confirm("Would you like Lowercases?");
+  if (includeLowercase) {
+    characters += lowercase;
+    alert("Your password will have Lowercase.");
   }
-  
+
+ var includeUppercase = confirm("Would you like Uppercase?");
+  if (includeUppercase) {
+    characters += uppercase;
+    alert("Your password will have Uppercase.");
+  }
+
+  if (characters.length === 0) {
+    return "Please select at least one character type.";
+  }
+
+
+ var password = "";
  for (var i = 0; i < passwordLength; i++) {
   password += characters[Math.floor(Math.random() * characters.length)];
  }
-
 
   return password;
 
