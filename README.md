e086542b1b31d92d8237476601aa239f4127cbb6
Junel <robin.lennuj@gmail.com>
2023-05-18 22:16:19 -0700

    Organizing & indenting the JS.

--- README.md
+++ README.md
@@ -4,19 +4,23 @@
 
 ## Description.
 * Simple, easy to use [Password Generator](https://junel-balbin.github.io/Password-Generator/).
+
 * For this challenge assignment.  I found it difficult to know where to start but in the process, I learned a lot but still need to study more to be confident in my abilities.
+
 * As a beginner to JavaScript.  I am unsure of how everything works but I am sure that I will learn more with time and practice.
 
 ## Installation.
 * On a web browser please follow the link to begin exploring. [Password Generator](https://junel-balbin.github.io/Password-Generator/).
+
 * Accessible on any mobile device and tablet.
 
 ## Usage.
 * Selecting Cancel = NO.  Selecting OK = YES.
 <img src="./assets/images/yn.png">
-* Selecting a number below length of 8 and numbers over 128 will give you a message. "IMPORTANT. PLEASE SELECT A LENGTH BETWEEN 8 - 128."
-* Inputting a letter instead of a number will also give a message.
-"IMPORTANT.  PLEASE SELECT A LENGTH BETWEEN 8 - 128."
+
+* It is highly recommended to refresh page when regenerating a new password.
+
+* Selecting a number below length of 8 or numbers over 128 will give you a message.  Inputting a letter instead of a number will also give a message. "IMPORTANT.  PLEASE SELECT A LENGTH BETWEEN 8 - 128."
 <img src="./assets/images/invalidlength.png">
 
 *  Selecting no character types will give you a message.
@@ -25,7 +29,9 @@
 
 ## Credits & Resources.
 * Idea snippets and notes from EdX and UCB.
+
 * Google search.  Stack Overflow search.  Youtube videos.
+
 * Rock, Paper, Scissor mini project was a big help and a great resource for applying the same principles and structure on building the generator.
 
 ## Contributing.
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code.
 var generateBtn = document.querySelector("#generate");
 
 // Character Arrays.
@@ -11,30 +11,28 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
  
 // Main Functions and conditional statements.
- function generatePassword() {
- 
-// Prompt for Password Length.  With "if, else if statements" and alerts.
- var passwordLength = prompt("How many characters would you like between 8 - 128?");
+function generatePassword() {
+  // Prompt for Password Length. With "if, else if statements" and alerts.
+  var passwordLength = prompt("How many characters would you like between 8 - 128?");
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-  return "IMPORTANT. PLEASE SELECT A LENGTH BETWEEN 8 - 128.";
-  }  else if (isNaN(passwordLength)) {
-     passwordLength = prompt("Please enter a valid number of length between 8 - 128.");
-  }
-     else {
-     alert("Your password will be " + passwordLength + " characters long.");
+    return "IMPORTANT. PLEASE SELECT A LENGTH BETWEEN 8 - 128.";
+  } else if (isNaN(passwordLength)) {
+    passwordLength = prompt("Please enter a valid number of length between 8 - 128.");
+  } else {
+    alert("Your password will be " + passwordLength + " characters long.");
   }
 
-// Confirm for Numbers.  With "if, else if statements" and alerts.
- var includeNumbers = confirm("Would you like numbers?");
+  // Confirm for Numbers. With "if, else if statements" and alerts.
+  var includeNumbers = confirm("Would you like numbers?");
   if (includeNumbers) {
-   characters += numbers;
-  alert("Your password will have numbers.");  
+    characters += numbers;
+    alert("Your password will have numbers.");  
   } else {
     alert("Your password will NOT have numbers.");
   }
 
-// Confirm for Special Characters.  With "if, else if statements" and alerts.
- var includeSpecialCharacters = confirm("Would you like Special Characters?");
+  // Confirm for Special Characters. With "if, else if statements" and alerts.
+  var includeSpecialCharacters = confirm("Would you like Special Characters?");
   if (includeSpecialCharacters) {
     characters += specialCharacters;
     alert("Your password will have Special Characters.");
@@ -42,8 +40,8 @@ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     alert("Your password will NOT have Special Characters.");
   }
   
-// Confirm for Lowercase.  With "if, else if statements" and alerts.
- var includeLowercase = confirm("Would you like Lowercases?");
+  // Confirm for Lowercase. With "if, else if statements" and alerts.
+  var includeLowercase = confirm("Would you like Lowercases?");
   if (includeLowercase) {
     characters += lowercase;
     alert("Your password will have Lowercase.");
@@ -51,40 +49,35 @@ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     alert("Your password will NOT have Lowercase.");
   }
 
-// Confirm for Uppercase.  With "if, else if statements" and alerts.
- var includeUppercase = confirm("Would you like Uppercase?");
+  // Confirm for Uppercase. With "if, else if statements" and alerts.
+  var includeUppercase = confirm("Would you like Uppercase?");
   if (includeUppercase) {
     characters += uppercase;
     alert("Your password will have Uppercase.");
   } else {
     alert("Your password will NOT have Uppercase.");
   }
-
+  
   if (characters.length === 0) {
     return "IMPORTANT. PLEASE SELECT AT LEAST ONE CHARACTER TYPE.";
   }
 
-// For-Loop with Math.floor & Math.random.
- var password = "";
- for (var i = 0; i < passwordLength; i++) {
-  password += characters[Math.floor(Math.random() * characters.length)];
- }
+  // For-Loop with Math.floor & Math.random.
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
 
   return password;
-
 }
 
-
-  // Write password to the #password input
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
